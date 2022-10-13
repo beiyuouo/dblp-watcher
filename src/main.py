4dae6aa3147a8b556879d51153d717873269a682
beiyuouo <bj.yan.pa@qq.com>
2022-10-13 16:02:50 +0800

    init

---
+++ src/main.py
@@ -0,0 +1,56 @@
+from loguru import logger
+from fire import Fire
+from utils import init, get_dblp_items
+import yaml
+import requests
+
+
+class Scaffold:
+    def __init__(self):
+        pass
+
+    def run(self, env: str = "dev", cfg: str = "./../config.yaml"):
+        cfg = init(cfg_path=cfg)
+
+        logger.info(f"running with env: {env} and cfg: {cfg}")
+
+        # dblp
+
+        # load cache
+        cache_path = cfg["cache_path"] / "dblp.yaml"
+        dblp_cache = (
+            yaml.safe_load(open(cache_path, "r")) if cache_path.exists() else {}
+        )
+        logger.info(f"dblp cache: {dblp_cache}")
+        dblp_new_cache = {}
+
+        dblp_url = cfg["dblp"]["url"]
+        mails = cfg["dblp"]["mails"]
+        for topic in cfg["dblp"]["topics"]:
+            logger.info(f"topic: {topic}, mails: {mails}")
+
+            # get dblp data
+            dblp_data = requests.get(dblp_url.format(topic)).json()
+            logger.info(f"dblp_data: {dblp_data}")
+
+            # get items
+            items = get_dblp_items(dblp_data)
+            logger.info(f"items: {items}")
+
+            # new cache
+            cached_items = dblp_cache.get(topic, [])
+            new_items = [item for item in items if item not in cached_items]
+            dblp_new_cache[topic] = new_items
+
+            if topic not in dblp_cache:
+                dblp_cache[topic] = []
+            dblp_cache[topic].extend(new_items)
+
+            logger.info(f"new_items: {new_items}")
+
+        # save cache
+        yaml.safe_dump(dblp_cache, open(cache_path, "w"), sort_keys=False, indent=2)
+
+
+if __name__ == "__main__":
+    Fire(Scaffold)
---
+++ src/utils.py
@@ -0,0 +1,83 @@
+import sys
+import os
+import yaml
+from loguru import logger
+from pathlib import Path
+import ezkfg as ez
+
+
+def init_log():
+    """Initialize loguru log information"""
+    event_logger_format = (
+        "<g>{time:YYYY-MM-DD HH:mm:ss}</g> | "
+        "<lvl>{level}</lvl> - "
+        # "<c><u>{name}</u></c> | "
+        "{message}"
+    )
+    logger.remove()
+    logger.add(
+        sink=sys.stdout,
+        colorize=True,
+        level="DEBUG",
+        format=event_logger_format,
+        diagnose=False,
+    )
+
+    return logger
+
+
+def init_path(cfg):
+    cfg["cache_path"] = Path("./../cached")
+
+    return cfg
+
+
+def init(cfg_path: str):
+    cfg = ez.Config().load(cfg_path)
+    cfg = init_path(cfg)
+    init_log()
+    return cfg
+
+
+def get_item_info(item, key):
+    try:
+        return item[key]
+    except KeyError:
+        return ""
+
+
+def get_dblp_items(dblp_data):
+    try:
+        items = dblp_data["result"]["hits"]["hit"]
+    except KeyError:
+        items = []
+
+    # item{'author', 'title', 'venue', 'year', 'type', 'access', 'key', 'doi', 'ee', 'url'}
+    res_items = []
+
+    for item in items:
+        res_item = {}
+        # format author
+        authors = get_item_info(item["info"], "authors")
+        authors = [author["text"] for author in authors["author"]]
+
+        logger.info(f"authors: {authors}")
+
+        res_item["author"] = ", ".join(authors)
+        needed_keys = [
+            "title",
+            "venue",
+            "year",
+            "type",
+            "access",
+            "key",
+            "doi",
+            "ee",
+            "url",
+        ]
+        for key in needed_keys:
+            res_item[key] = get_item_info(item["info"], key)
+
+        res_items.append(res_item)
+
+    return res_items
